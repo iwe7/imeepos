0a75aefccdceeb00597482f7f741797b40809eb7
meepobrother <1037483576@qq.com>
2018-08-31 13:42:57 +0800

    update

--- addons/ims-demo/src/main.ts
+++
@@ -1,4 +0,0 @@
-import { from } from 'rxjs';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './app/app.module';
-export const platform = from(platformBrowserDynamic().bootstrapModule(AppModule));
